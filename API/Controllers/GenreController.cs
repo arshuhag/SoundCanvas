e2a3c33cf967153fbb06460cdc80485521947ecc
Md. Anisur Rahaman Shuhag <arshuhag.cse@gmail.com>
2024-10-29 00:28:47 +0600

    Add project files.

---
+++ API/Controllers/GenreController.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly ApplicationDb _db;
+
+        public GenreController(ApplicationDb db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("get-all")]
+
+        public IActionResult GetAll()
+        {
+            return Ok(_db.Genres.ToList());
+        }
+    }
+}
